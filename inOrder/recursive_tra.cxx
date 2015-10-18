5d676a5f07ad166ee4d0f44da8738f15cb935bc1
ryt <rytubuntulinux@sina.com>
2015-10-18 08:01:42 +0800

    update

---
+++ inOrder/recursive_tra.cxx
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stack>
+using std::endl;
+using std::cout;
+
+struct Node
+{
+	int data = 0;
+	Node *lchild = NULL;
+	Node *rchild = NULL;
+};
+
+Node* generateNode(int value)
+{
+	Node *newNode = new Node;
+	newNode->data = value;
+	return newNode;
+}
+
+void destroy(Node **root)
+{
+	if((*root) != NULL && (*root)->lchild != NULL)
+		destroy(&(*root)->lchild);
+	if((*root) != NULL && (*root)->rchild != NULL)
+		destroy(&(*root)->rchild);
+	free(*root);
+	*root = NULL;
+}
+
+/**************** Recursive Traversal *******************/
+void inOrder(Node *root)     
+{
+	if(root != NULL)
+	{
+		inOrder(root->lchild);
+		cout<<root->data<<" ";
+		inOrder(root->rchild);
+	}
+}
+/******************************************************/
+
+int main()
+{
+	/*   Constructed binary tree is
+	            1
+			   / \
+			  2   3
+			 /   / \
+			4   5   6
+			 \
+			  7
+	*/
+	Node *root = generateNode(1);
+	root->lchild = generateNode(2);
+	root->rchild = generateNode(3);
+	root->lchild->lchild = generateNode(4);
+	root->lchild->lchild->rchild = generateNode(7);
+	root->rchild->lchild = generateNode(5);
+	root->rchild->rchild = generateNode(6);
+	inOrder(root);
+	cout<<endl;
+	destroy(&root);
+	return 0;
+}
